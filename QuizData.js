7ca4689440a4ad49b7ac7f660f52750a7bbf85ab
Jyotsna Joshi <86952338+JyotsnaJoshi24@users.noreply.github.com>
2024-02-28 11:07:45 +0530

    Add files via upload

---
+++ QuizData.js
@@ -0,0 +1,27 @@
+export const QuizData = [
+    {
+        question : "which language runs in a web browser?"
+        , options :["Java", " C" , "Python","Javascript"],
+        answer: 4
+    },
+    {
+        question : "We enclose HTML tags within?"
+        , options : ["\\", "!!" ,"{}", "<>"],
+        answer : 4
+    },
+    {
+        question : "How many sizes of headers are available in HTML by default?"
+        ,options : ["5" ,"1" ,"3", "6"],
+        answer : 4
+    },
+    {
+        question : "What is the smallest header in HTML by default?"
+        ,options  : ["h1", "h2", "h6" ,"h4"],
+        answer : 3
+    },
+    {
+        question :"How to create an ordered list in HTML?"
+        ,options : ["<ul>", "<ol>", "<il>", "<href"],
+        answer : 2
+    },
+]
